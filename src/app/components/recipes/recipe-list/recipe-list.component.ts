18bac63b2dfc8524130c53b776a407ddf86f4c33
Mario Serrano <maseflores79@gmail.com>
2018-03-04 07:30:01 -0800

    Agregada funcionalidad de test de recetas

--- src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Recipe} from '../recipe.model';
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+    new Recipe('A test recipe',
+               'This is a test',
+              'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'),
+    new Recipe('una recetita',
+               'Otro test',
+      'https://www.gallinablanca.es/files/thumbs/807e922ac87a1e4bb8f5a835bdf86a0f7c3af799_r900_340_5.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
