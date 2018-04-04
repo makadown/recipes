a435f108f29e5805a3d50bd3c08ad74492180ba2
Mario Serrano <maseflores79@gmail.com>
2018-04-03 20:32:19 -0700

    Se agregó preloadingStrategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { ShoppingListComponent } from './components/shopping-list/shopping-list.component';
 import { SignupComponent } from './auth/signup/signup.component';
-import { SigninComponent } from './auth/signin/signin.component'; 
+import { SigninComponent } from './auth/signin/signin.component';
 import { HomeComponent } from './core/home/home.component';
 
 const app_routes: Routes = [
@@ -12,11 +12,10 @@ const app_routes: Routes = [
   { path: 'shopping-list', component: ShoppingListComponent }
 ];
 
+/* Con preloadingStrategy : PreloadAllModules  precargo los modulos que se cargarán lazy */
 @NgModule({
-  imports: [RouterModule.forRoot(app_routes)],
-  exports: [RouterModule],
-  declarations: [],
-  providers: []
+  imports: [RouterModule.forRoot(app_routes, { preloadingStrategy : PreloadAllModules })],
+  exports: [RouterModule]
 })
 export class AppRoutingModule{
 
